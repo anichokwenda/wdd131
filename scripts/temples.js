cda235b4b2313de049e95538108b797497c9f108
anichokwenda <anichokwenda@gmail.com>
2025-07-12 15:04:00 +0200

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,16 +1,5 @@
-const currentYearElement =document.getElementById("currentyear");
-const lastModifiedElement = document.getElementById("lastModified");
-
-if (currentYearElement && lastModifiedElement){
-    currentYearElement.textContent= new Date().getFullYear();
-    const lastModifiedDate = new Date(document.lastModified);
-    const options = { year:'numeric', month:'2-digit', day: '2-digit', hour:'2-digit', minute: '2-digit', second:'2-digit'};
-    lastModifiedElement.textContent = 'Last Modified: ${lastModifiedDate.toLocaleString(undefined, options)}';
-} else{
-    console.error("One or both elements not found");
-}
-
-
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
 document.querySelectorAll('nav a').forEach((item) => {
     item.addEventListener('click', (e) => {
