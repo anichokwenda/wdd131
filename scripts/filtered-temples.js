f9d24861a233094ef3f8763cf553c0911871eb11
anichokwenda <anichokwenda@gmail.com>
2025-07-26 13:46:51 +0200

    changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -129,18 +129,21 @@ const temples = [
 ];
 
 const main = document.querySelector("main");
-temples.forEach(temple=>{
+const resGrid = document.querySelector(".res-grid");
+
+if (resGrid){
+  temples.forEach(temple=>{
     let card= document.createElement("section");
     let name= document.createElement("h3");
     let location= document.createElement("p");
     let dedication= document.createElement("p");
-    let area= document.createElement("p");
-    let img= document.createElement("img");
+    let area = document.createElementById("p");
+    let img = document.createElement("img");
 
     name.textContent = temple.templeName;
-    location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
-    dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
-    area.innerHTML=`span class="label">Size:</span> ${temple.area} sq ft`;
+    location.innerHTML=`<span class="label">Location:</span> ${temple.location}`;
+    dedication.innerHTML=`<span class="label">Dedicated:</span> ${temple.dedicated}`;
+    area.innerHTML=`<span class="label">Size:</span> ${temple.area} aq ft`;
     img.setAttribute("src", temple.imageUrl);
     img.setAttribute("alt", `${temple.templeName} Temple`);
     img.setAttribute("loading", "lazy");
@@ -150,9 +153,14 @@ temples.forEach(temple=>{
     card.appendChild(dedication);
     card.appendChild(area);
     card.appendChild(img);
+    resGrid.appendChild(card);
 
-    document.querySelector(".res-grid").appendChild(card);
-});
+  });
+}
+  else{
+    console.error("Element with class 'res-grid' not found.")
+  }
+
 
 
 
